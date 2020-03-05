bb53d8ac04fad1d81a0006c7d7a0a5d263ff8223
HeartPattern <jun2620354@kaist.ac.kr>
2020-03-05 21:55:49 +0900

    항상 mavenLocal을 추가하도록 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 group = "kr.heartpattern.spikot"
 version = "1.0-SNAPSHOT"
 
-// For faster build in jenkins ci
-if("nexusUser" in properties && "nexusPassword" in properties){
-    allprojects{
-        repositories{
-            maven("https://maven.heartpattern.kr/repository/minecraft-snapshots/"){
-                credentials{
+allprojects {
+    repositories {
+        mavenLocal()
+
+        // For faster build in jenkins ci
+        if("nexusUser" in properties && "nexusPassword" in properties) {
+            maven("https://maven.heartpattern.kr/repository/minecraft-snapshots/") {
+                credentials {
                     username = project.properties["nexusUser"].toString()
                     password = project.properties["nexusPassword"].toString()
                 }
